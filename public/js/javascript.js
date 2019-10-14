59d75ce75592cbfe0ce7daf2913c09f7531248d9
Nehal Singla <nehalsingla24@gmail.com>
2019-10-14 10:24:23 +0530

    submit button

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,9 +1,10 @@
+var code = document.getElementById("code").contentWindow.document;
+  
 function compile() {
     var html = document.getElementById("html");
     var css = document.getElementById("css");
     var js = document.getElementById("js");
-    var code = document.getElementById("code").contentWindow.document;
-  
+    
     document.body.onkeyup = function() {
       code.open();
       code.writeln(
@@ -20,3 +21,15 @@ function compile() {
   }
   
   compile();
+
+  function submit_data()
+  {
+    console.log(code);
+    if(html.value=='')
+    {
+      alert("Html fields are empty");
+      return;
+    }
+
+
+  }
